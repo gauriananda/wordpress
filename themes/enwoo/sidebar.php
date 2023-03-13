39e631fc99e533d55ce84d6cd9f5bf04f063b8ab
gauri <gaurianandaa@gmail.com>
2023-03-13 09:52:33 +0800

    wordpress

--- themes/enwoo/sidebar.php
+++
@@ -1,6 +0,0 @@
-<?php if (is_active_sidebar('enwoo-right-sidebar')) { ?>
-    <aside id="sidebar" class="col-md-3">
-        <?php dynamic_sidebar('enwoo-right-sidebar'); ?>
-    </aside>
-<?php 
-}
